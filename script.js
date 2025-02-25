9f96168b516d31e03b99581f3eefa653704943de
DinhNguyen30309 <tonny30309@gmail.com>
2025-02-25 07:25:23 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+function login()
+{
+  document.getElementById(compiler).style.display="block"
+  let u = username.value
+  let p = pw.value
+  if (u=="dinhnguyen" && p"123")
+  {
+    window.open()
+  }
+}
+    
